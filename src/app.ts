a237888f46da47a906ffd79e57eb3bafe88e8536
Carlos Duarte <carlosduarteinfo@gmail.com>
2026-01-14 16:50:23 -0300

    feat: implement error handling middleware and custom error classes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import Express from 'express';
+import { errorHandler } from './shared/middlewares/error-handler.js';
 
 export function appFactory() {
   const app = Express();
+
+  app.use(Express.json());
+
+  app.use(errorHandler);
 
   return app;
 }
