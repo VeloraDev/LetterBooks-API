fa74eddf3904c61cbf96335a8ef45bccd3cc3cd0
Carlos Duarte <carlosduarteinfo@gmail.com>
2026-01-27 20:29:43 -0300

    feat: add cookie parser to access cookies in request

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import Express from 'express';
 import { errorHandler } from './shared/middlewares/error-handler.js';
 import { authRouter } from './modules/auth/auth.routes.js';
+import cookieParser from 'cookie-parser';
 
 export function appFactory() {
   const app = Express();
-
   app.use(Express.json());
+  app.use(cookieParser());
 
   app.use('/auth', authRouter);
 
